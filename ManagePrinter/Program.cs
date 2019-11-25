3530f05303e9a449684fbe715552f11ba47f8b71
Iosmany <iosmany@novaengel.com>
2019-11-25 15:17:19 +0100

    initial upload

---
+++ ManagePrinter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Management;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagePrinter.Core.PrintersManagement.GetAllPrinters();
+
+
+
+        }
+    }
+}
